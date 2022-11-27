cbd29e41b763d15e16a098799fb0b4e62ad035f2
aulinka <laura.mikusova27@gmail.com>
2022-11-27 19:12:33 +0100

    initial commit:

---
+++ src/main/java/sk/umb/luiza/LuizaApplication.java
@@ -0,0 +1,13 @@
+package sk.umb.luiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizaApplication.class, args);
+		System.out.println("cingi lingi bom");
+	}
+}
---
+++ src/test/java/sk/umb/luiza/LuizaApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.umb.luiza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
